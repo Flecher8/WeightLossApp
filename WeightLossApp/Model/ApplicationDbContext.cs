9ed99f24876c19cc4ef692eaa40e32da3d5e2bae
DESKTOP-KTKC2M6\vmlf <vmlf.prudius@gmail.com>
2022-06-02 13:05:22 +0300

    Installed required packages

---
+++ WeightLossApp/Model/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Model
+{
+    internal class ApplicationDbContext : DbContext
+    {
+
+
+    }
+
+}
---
+++ WeightLossApp/Model/Models/IngridientData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WeightLossApp.Models
+{
+    public class IngridientData
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public float Calories { get; set; }
+        public float Proteins { get; set; }
+        public float Carbohydrates { get; set; }
+        public float Fats { get; set; }
+
+    }
+}
