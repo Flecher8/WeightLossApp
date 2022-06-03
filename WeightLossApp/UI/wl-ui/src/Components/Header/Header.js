29c3a7044933be7cd15c9d76cfd508f90176cd4b
Vlad Bocharov <vladislavboc11@gmail.com>
2022-06-03 22:44:37 +0300

    Added changes to clean the code

---
+++ WeightLossApp/UI/wl-ui/src/Components/Header/Header.js
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+	return (
+		<nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+			<Link className="navbar-brand mx-3" to={"/"}>
+				Weight Loss App
+			</Link>
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNavDropdown"
+				aria-controls="navbarNavDropdown"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNavDropdown">
+				<ul className="navbar-nav">
+					<li className="nav-item">
+						<Link className="nav-link" to="/IngridientsData">
+							Ingridients Data
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link className="nav-link" to={"/Exercises"}>
+							Features
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link className="nav-link" to={"/"}>
+							Pricing
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+}
+
+export default Header;
--- WeightLossApp/UI/wl-ui/src/index.js
+++ WeightLossApp/UI/wl-ui/src/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { Home } from "./Components/Home/Home";
+import { IngridientsData } from "./Components/IngridientsData/IngridientsData";
+import Header from "./Components/Header/Header";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // Entry point of application, adds App component to the index.html file
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/IngridientsData" element={<IngridientsData />} />
+
+				{/* Default Router */}
+				<Route path="/*" element={<Navigate to="/" />} />
+			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>
 );
