bbab5f61f13fb53cd1a779ad09a9a52f71e02a71
DESKTOP-KTKC2M6\vmlf <vmlf.prudius@gmail.com>
2022-06-05 19:05:36 +0300

    Little fixes in model

--- WeightLossApp/Model/Models/Exercise.cs
+++ WeightLossApp/Model/Models/Exercise.cs
@@ -13,14 +13,12 @@ namespace Model.Models
         public int Id { get; set; }
         public string Section { get; set; }
         public string Name { get; set; }
-        public DateTime Length { get; set; }
+        public int Length { get; set; }
         public string Instructions { get; set; }
         public byte[] ImageName { get; set; }
         public int BurntCalories { get; set; }
         public int NumberOfReps { get; set; }
-        public int TrainingId { get; set; }
 
-        public virtual Training Training { get; set; }
         public virtual ICollection<TrainingExercise> TrainingExercise { get; set; }
     }
 }
