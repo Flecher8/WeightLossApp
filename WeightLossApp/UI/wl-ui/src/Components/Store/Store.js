1531c99d6356bd8145ae79bec669c3b618709e0d
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-06 20:23:56 +0200

    Added constructor to the store react

---
+++ WeightLossApp/UI/wl-ui/src/Components/Store/Store.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { constants } from "../../Constants";
+import BootstrapTable from "react-bootstrap-table-next";
+import filterFactory, { textFilter, numberFilter } from "react-bootstrap-table2-filter";
+import paginationFactory from "react-bootstrap-table2-paginator";
+
+export class StoreData extends Component {
+	// Main constructor
+	constructor(props) {
+		super(props);
+
+		// State initialization
+		// Component state - properties of this component
+		this.state = {
+			// List of data to be displayed
+			PremiumSubscription: [],
+            DesignTheme: [],
+			// Title of the modal window
+			modalTitle: "",
+
+			// Data of the PremiumSubscription item
+			// This data will be displayed in the modal window
+			premiumID: 0,
+			premiumDays: 0,
+			premiumPrice: 0.0,
+
+            // Data of the DesignThemeData item
+			// This data will be displayed in the modal window
+			themeID: 0,
+			themeImage: "",
+			themeBaseColor: "",
+			themeAccentColor: "",
+			themeSecondaryColor: "",
+		};
+	}
+}
