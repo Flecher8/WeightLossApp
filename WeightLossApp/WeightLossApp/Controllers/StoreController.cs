26b62ae7ef0104d151d22985d8c09b3b8a3a6976
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-06 18:35:26 +0200

    Updated with constructor and get methods

---
+++ WeightLossApp/WeightLossApp/Controllers/StoreController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.Models;
+
+namespace WeightLossApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        // DataBase context
+        private readonly FitnessAssistantContext _context;
+
+        public StoreController(FitnessAssistantContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public JsonResult GetPremiums()
+        {
+            return new JsonResult(_context.PremiumSubscription);
+        }
+
+        [HttpGet]
+        public JsonResult GetDesignThemes()
+        {
+            return new JsonResult(_context.DesignThemeData);
+        }
+    }
+}
