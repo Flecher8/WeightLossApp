8eeb8542f136957c87a64fc30f24f93ac7c4d4ef
Vlad Bocharov <vladislavboc11@gmail.com>
2022-06-08 19:35:02 +0300

    Added useState to store SectionTraining data

--- WeightLossApp/UI/wl-ui/src/Components/SectionTraining/SectionTraining.js
+++ WeightLossApp/UI/wl-ui/src/Components/SectionTraining/SectionTraining.js
@@ -3,6 +3,10 @@ import React from "react";
 import { constants } from "../../Constants";
 import { Modal, Button, Form, Row, Col, Table, InputGroup, FormControl } from "react-bootstrap";
 
-function SectionTraining() {}
+function SectionTraining() {
+	// SectionTraining data
+	const [sectionTraining, setSectionTraining] = useState([]);
+	const [Section, setSection] = useState();
+}
 
 export default SectionTraining;
