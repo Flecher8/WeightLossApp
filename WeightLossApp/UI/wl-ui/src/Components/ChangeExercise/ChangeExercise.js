f8af4be093d9fe8920763aa0577a726aee2911ee
Vlad Bocharov <vladislavboc11@gmail.com>
2022-06-08 15:07:20 +0300

    Created UI component ChangeExercise

---
+++ WeightLossApp/UI/wl-ui/src/Components/ChangeExercise/ChangeExercise.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import { constants } from "../../Constants";
+import { Modal, Button, Form, Row, Col } from "react-bootstrap";
+
+function ChangeExercise(props) {}
+
+export default ChangeExercise;
