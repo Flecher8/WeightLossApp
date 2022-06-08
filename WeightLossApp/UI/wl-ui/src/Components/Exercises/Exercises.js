00c00091944a810a65fb6abc600e6d8088331f75
Vlad Bocharov <vladislavboc11@gmail.com>
2022-06-08 14:57:09 +0300

    Added Route with path /Exercises in index.js

--- WeightLossApp/UI/wl-ui/src/Components/Exercises/Exercises.js
+++ WeightLossApp/UI/wl-ui/src/Components/Exercises/Exercises.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import React from "react";
-import constants from "../../constants";
+import { constants } from "../../Constants";
 import { Modal, Button, Form, Row, Col, Table, InputGroup, FormControl } from "react-bootstrap";
 
 function Exercises() {
