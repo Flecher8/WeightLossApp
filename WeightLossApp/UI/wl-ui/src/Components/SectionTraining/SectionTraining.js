f787ee2a731d5e12b60f0bc0d171420f96a05090
Vlad Bocharov <vladislavboc11@gmail.com>
2022-06-08 19:33:24 +0300

    Created UI component SectionTraining

---
+++ WeightLossApp/UI/wl-ui/src/Components/SectionTraining/SectionTraining.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import React from "react";
+import { constants } from "../../Constants";
+import { Modal, Button, Form, Row, Col, Table, InputGroup, FormControl } from "react-bootstrap";
+
+function SectionTraining() {}
+
+export default SectionTraining;
