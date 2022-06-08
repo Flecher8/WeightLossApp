388e7d3d68a76d758131cda330b8aa488cd9bf80
Vlad Bocharov <vladislavboc11@gmail.com>
2022-06-08 14:45:14 +0300

    Created function Exercises and added necessary moduls from react and constants

---
+++ WeightLossApp/UI/wl-ui/src/Components/Exercises/Exercises.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+import React from "react";
+import variables from "../../Variables";
+import { Modal, Button, Form, Row, Col, Table, InputGroup, FormControl } from "react-bootstrap";
+import ChangeExercise from "../ChangeExercise/ChangeExercise";
+
+function Exercises() {}
+
+export default Exercises;
