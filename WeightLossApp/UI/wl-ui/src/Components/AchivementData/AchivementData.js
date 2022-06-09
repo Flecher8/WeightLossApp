3e3d29db72f0fc3b3a7790397835ffb0e1673ea3
Vlad Bocharov <vladislavboc11@gmail.com>
2022-06-09 13:40:00 +0300

    Added useState variables for AchivementData data

--- WeightLossApp/UI/wl-ui/src/Components/AchivementData/AchivementData.js
+++ WeightLossApp/UI/wl-ui/src/Components/AchivementData/AchivementData.js
@@ -3,6 +3,10 @@ import React from "react";
 import { constants } from "../../Constants";
 import { Modal, Button, Form, Row, Col, Table, InputGroup, FormControl } from "react-bootstrap";
 
-function AchivementData() {}
+function AchivementData() {
+	// AchivementData data
+	const [achivementData, setAchivementData] = useState([]);
+	const [achivement, setAchivement] = useState();
+}
 
 export default AchivementData;
