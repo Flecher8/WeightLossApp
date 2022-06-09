1ecfa90195aa6414b5b965a75913aa5e465e34ba
Vlad Bocharov <vladislavboc11@gmail.com>
2022-06-09 13:59:42 +0300

    Created UI component ChangeAchivement

---
+++ WeightLossApp/UI/wl-ui/src/Components/ChangeAchivement/ChangeAchivement.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import { constants } from "../../Constants";
+import { Modal, Button, Form, Row, Col } from "react-bootstrap";
+
+function ChangeAchivement(props) {}
+
+export default ChangeAchivement;
