45641df4f630421437dbe89cd97fb2045a626689
Vlad Bocharov <vladislavboc11@gmail.com>
2022-06-09 13:38:11 +0300

    Created new UI component AchivementData

---
+++ WeightLossApp/UI/wl-ui/src/Components/AchivementData/AchivementData.js
@@ -0,0 +1,8 @@
+import { useState, useEffect } from "react";
+import React from "react";
+import { constants } from "../../Constants";
+import { Modal, Button, Form, Row, Col, Table, InputGroup, FormControl } from "react-bootstrap";
+
+function AchivementData() {}
+
+export default AchivementData;
