2e1f12e4869de1aac38fdff5dab74b56e03de384
Vlad Bocharov <vladislavboc11@gmail.com>
2022-06-11 13:21:52 +0300

    Added variables to show add training modal and edit training modal

--- WeightLossApp/UI/wl-ui/src/Components/Trainings/Trainings.js
+++ WeightLossApp/UI/wl-ui/src/Components/Trainings/Trainings.js
@@ -7,6 +7,14 @@ function Trainings() {
 	// Trainings data
 	const [trainings, setTrainings] = useState([]);
 	const [training, setTraining] = useState();
+
+	const [addShow, setAddShow] = useState(false);
+	const addHandleClose = () => setAddShow(false);
+	const addHandleShow = () => setAddShow(true);
+
+	const [editShow, setEditShow] = useState(false);
+	const editHandleClose = () => setEditShow(false);
+	const editHandleShow = () => setEditShow(true);
 }
 
 export default Trainings;
