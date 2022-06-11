bfb58f3875f2c563a05a1513c95f8a1bea9d5f59
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-11 22:18:16 +0200

    Started DesignTheme function in js file + added necessary imports

---
+++ WeightLossApp/UI/wl-ui/src/Components/DesignTheme/DesignTheme.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { constants } from "../../Constants";
+import BootstrapTable from "react-bootstrap-table-next";
+import filterFactory, { textFilter, numberFilter } from "react-bootstrap-table2-filter";
+import paginationFactory from "react-bootstrap-table2-paginator";
+
+function DesignTheme() {
+
+}
+
+export default DesignTheme;
