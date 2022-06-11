eb4740ed6c6bff099a49acd68e9859da59bdd56c
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-11 22:21:40 +0200

    Changed imports + added design theme data variables

--- WeightLossApp/UI/wl-ui/src/Components/DesignTheme/DesignTheme.js
+++ WeightLossApp/UI/wl-ui/src/Components/DesignTheme/DesignTheme.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
+import { useState, useEffect } from "react";
+import React from "react";
 import { constants } from "../../Constants";
-import BootstrapTable from "react-bootstrap-table-next";
-import filterFactory, { textFilter, numberFilter } from "react-bootstrap-table2-filter";
-import paginationFactory from "react-bootstrap-table2-paginator";
+import { Modal, Button, Form, Row, Col, Table, InputGroup, FormControl } from "react-bootstrap";
 
 function DesignTheme() {
+    // Design theme data
+    const [designThemes, setExercises] = useState([]);
+    const [designTheme, setExercise] = useState();
 
 }
 
