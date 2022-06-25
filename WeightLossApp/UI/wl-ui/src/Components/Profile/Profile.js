64e362464bb454be73eb10ab6cc64424d9c8107c
Prudius Vlad <73965757+Prud1usVlad@users.noreply.github.com>
2022-06-25 14:00:02 +0300

    Revert "Deleting junk from dev"

--- WeightLossApp/UI/wl-ui/src/Components/Profile/Profile.js
+++
@@ -1,5 +0,0 @@
-import { useState, useEffect } from "react";
-import React from "react";
-import { constants } from "../../Constants";
-import { Modal, Button, Form, Row, Col, Table, InputGroup, FormControl } from "react-bootstrap";
-
--- WeightLossApp/WeightLossApp/Startup.cs
+++ WeightLossApp/WeightLossApp/Startup.cs
@@ -5,16 +5,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using Newtonsoft.Json.Serialization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.FileProviders;
-using System.IO;
-using Model.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace WeightLossApp
 {
@@ -30,31 +23,12 @@ namespace WeightLossApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //Enable CORS
-            services.AddCors(c =>
-            {
-                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-            });
-
-            services.AddDbContext<FitnessAssistantContext>(opts =>
-                opts.UseSqlServer(Configuration.GetConnectionString("sqlConnection")));
-
-
-            //JSON Serializer
-            services.AddControllersWithViews().AddNewtonsoftJson(options =>
-            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
-                .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
-                = new DefaultContractResolver());
-
-            services.AddControllers();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //Enable CORS
-            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
