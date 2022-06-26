ec9b0d9a6405f0288ba7003d0e765ba9520804a9
Vlad Bocharov <vladislavboc11@gmail.com>
2022-06-26 22:23:50 +0300

    Created class AddEventVM

---
+++ WeightLossApp/Sandbox/AddEventVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Mobile.Helpers;
+using Mobile.Models;
+using Xamarin.Forms;
+using Newtonsoft.Json.Linq;
+using Xamarin;
+
+namespace Sandbox
+{
+    public class AddEventVM : PropertyChangedIpmlementator
+    {
+    }
+}
