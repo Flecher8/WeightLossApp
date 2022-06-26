e7f3175381f007ba15bdd43167871f7e92be7d92
irynabotsiura <iryna.botsiura@nure.ua>
2022-06-26 19:23:30 +0300

    added MainPageVM to Sandbox and configured calculation of nutrition progress relying on appProfile data

--- WeightLossApp/Mobile/Mobile/Models/Member.cs
+++ WeightLossApp/Mobile/Mobile/Models/Member.cs
@@ -14,9 +14,8 @@ namespace Mobile.Models
         public double Weight { get; set; }
         public double Height { get; set; }
         public string Goal { get; set; }
-        public DateTime? Birthday { get; set; }
+        public DateTime Birthday { get; set; }
         public DateTime RegistrationDate { get; set; }
-
-        
+        public string Gender { get; set; }
     }
 }
