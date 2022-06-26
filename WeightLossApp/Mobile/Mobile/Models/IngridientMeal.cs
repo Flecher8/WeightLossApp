67c0c4eb2ea462a2ff8787bbc96dac072bf5a9ae
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-26 20:00:03 +0300

    Added IngidientMeal model and base version of AddMealVM

---
+++ WeightLossApp/Mobile/Mobile/Models/IngridientMeal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mobile.Models
+{
+    public class IngridientMeal
+    {
+        public string IngridientName { get; set; }
+        public string DayActivityMeal { get; set; }
+        public string IngridientId { get; set; }
+
+        public double Weight { get; set; }
+    }
+}
