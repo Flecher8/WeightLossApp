cfd85bbaa56c085d05bc71e1aef33665a221c9d5
DESKTOP-KTKC2M6\vmlf <vmlf.prudius@gmail.com>
2022-06-26 16:45:01 +0300

    Meals page content added

---
+++ WeightLossApp/Mobile/Mobile/Views/MealsPageContent.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Mobile.Models;
+
+namespace Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MealsPageContent : ContentView
+    {
+        public List<Meal> Meals { get; set; }
+
+        public MealsPageContent()
+        {
+            InitializeComponent();
+
+            Meals = new List<Meal>()
+            {
+                new Meal()
+                {
+                    Name = "Dinner",
+
+                },
+                new Meal()
+                {
+                    Name = "Lunch",
+
+                }
+            };
+
+
+            BindingContext = this;
+        }
+
+        private void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            ((ListView)sender).SelectedItem = null;
+        }
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    
+}
