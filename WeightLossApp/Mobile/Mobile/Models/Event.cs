77aa5ccee98bf06c42290696a1a335f0704f3e0d
Vlad Bocharov <vladislavboc11@gmail.com>
2022-06-26 18:39:46 +0300

    Added class Event in Mobile.Models

---
+++ WeightLossApp/Mobile/Mobile/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mobile.Models
+{
+    public class Event
+    {
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public bool SendNotification { get; set; }
+        public int ScheduleId { get; set; }
+        public DateTime DataTime { get; set; }
+    }
+}
