30c04c0aff5bd2fbd182055dd4b35d867cda135c
Vlad Bocharov <vladislavboc11@gmail.com>
2022-06-26 22:24:51 +0300

    Fill AddEventVM class

--- WeightLossApp/Sandbox/AddEventVM.cs
+++ WeightLossApp/Sandbox/AddEventVM.cs
@@ -17,5 +17,96 @@ namespace Sandbox
 {
     public class AddEventVM : PropertyChangedIpmlementator
     {
+        // Data
+        private ObservableCollection<string> categories;
+        private string selectedCategory;
+        private string messageText;
+        private DateTime date;
+        private DateTime time;
+
+
+        // Commands
+        public Command Back { get; private set; }
+        public Command Create { get; private set; }
+
+        // Navigation
+        public INavigation Navigation { get; set; }
+
+        public AddEventVM()
+        {
+            // Create data
+            categories = new ObservableCollection<string>();
+            fillCategories();
+            date = DateTime.Today;
+            time = DateTime.Now;
+
+            // Set commands
+            Back = new Command(goBack);
+            Create = new Command(CreateEvent);
+        }
+
+        // List of categories
+        public ObservableCollection<string> Categories
+        {
+            get => categories;
+            set
+            {
+                categories = value;
+                OnPropertyChanged();
+            }
+        }
+        public string SelectedCategory
+        {
+            get => selectedCategory;
+            set
+            {
+                selectedCategory = value;
+                OnPropertyChanged();
+            }
+        }
+        public string MessageText
+        {
+            get => messageText;
+            set
+            {
+                messageText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime Date
+        {
+            get => date;
+            set
+            {
+                date = value;
+                OnPropertyChanged();
+            }
+        }
+        public DateTime Time
+        {
+            get => time;
+            set
+            {
+                time = value;
+                OnPropertyChanged();
+            }
+        }
+        private void goBack()
+        {
+            Navigation.PopAsync();
+        }
+        // Need new functions!!!
+        private void CreateEvent()
+        {
+            // 1) Post new event !!!
+            // 2) Navigate to last page ( use goBack function)
+        }
+        private void fillCategories()
+        {
+            categories.Add("Food");
+            categories.Add("Training");
+            categories.Add("Medicine");
+        }
     }
 }
