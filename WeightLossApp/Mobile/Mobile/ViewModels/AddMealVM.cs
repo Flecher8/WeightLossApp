4935cc1d424c81addfbb8461c44d81fcc78607a4
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-27 13:26:15 +0300

    Added meal service to AddMealVM + filled OnAddClick with fake logic

--- WeightLossApp/Mobile/Mobile/ViewModels/AddMealVM.cs
+++ WeightLossApp/Mobile/Mobile/ViewModels/AddMealVM.cs
@@ -5,20 +5,30 @@ using System.Text;
 using System.Windows.Input;
 using Mobile.Helpers;
 using Mobile.Models;
+using Mobile.Services;
 using Xamarin.Forms;
 
 namespace Mobile.ViewModels
 {
     public class AddMealVM : PropertyChangedIpmlementator
     {
+        // services
+        private readonly IngredientMealService _mealService;
+
         // data fields
         private ObservableCollection<IngridientMeal> _mealIngridients;
         private string _mealName;
 
         // commands properties
-        public Command CreateMeal; 
+        public Command CreateMeal;
         public Command AddIngredient => new Command(OnAddClick);
         public Command<IngridientMeal> RemoveIngredientCommand => new Command<IngridientMeal>(OnRemoveClick);
+
+        public AddMealVM()
+        {
+            _mealService = new IngredientMealService();
+            _mealIngridients = new ObservableCollection<IngridientMeal>();
+        }
 
         // data properties
         public ObservableCollection<IngridientMeal> MealIngridients
@@ -43,7 +53,10 @@ namespace Mobile.ViewModels
 
         private void OnAddClick()
         {
-
+            foreach (var ingridientMeal in _mealService.ConvertIngredientDataToMealParts(new List<IngridientData>()))
+            {
+                _mealIngridients.Add(ingridientMeal);
+            }
         }
 
         private void OnRemoveClick(object obj)
