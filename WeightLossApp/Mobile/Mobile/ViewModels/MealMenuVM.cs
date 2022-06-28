8fc951eaaa1e068e81ade33ed4d32882cd9714df
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-28 11:36:49 +0300

    Added observable meal collection + ctor + meal service instance to MealMenuVM

--- WeightLossApp/Mobile/Mobile/ViewModels/MealMenuVM.cs
+++ WeightLossApp/Mobile/Mobile/ViewModels/MealMenuVM.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using Mobile.Helpers;
+using Mobile.Models;
+using Mobile.Services;
 
 namespace Mobile.ViewModels
 {
-    public class MealMenuVM
+    public class MealMenuVM : PropertyChangedIpmlementator
     {
+        // services
+        private readonly MealService _mealService;
 
+        // data
+        private ObservableCollection<Meal> _meals;
+
+        public MealMenuVM()
+        {
+            _mealService = new MealService();
+        }
+
+        public ObservableCollection<Meal> Meals
+        {
+            get => _meals;
+            set
+            {
+                _meals = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
