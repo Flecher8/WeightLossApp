a2b51c56b403e431d7fe13c3b7e11b80feb3aede
TellFruit <ju1c7.d1g1t2@gmail.com>
2022-06-28 08:56:07 +0300

    Adde post method to call creation of meal data instance

--- WeightLossApp/Mobile/Mobile/Services/IngredientMealService.cs
+++ WeightLossApp/Mobile/Mobile/Services/IngredientMealService.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
 using Mobile.Models;
 
 namespace Mobile.Services
@@ -17,6 +21,27 @@ namespace Mobile.Services
                 IngridientName = ingredientData.Name,
                 Weight = "100"
             }).ToList();
+        }
+
+        public IEnumerable<IngridientMeal> GetOnlyUnknownIngredients(IEnumerable<IngridientMeal> initMealIngredients, IEnumerable<IngridientMeal> newMealIngredients)
+        {
+            return initMealIngredients.Where(i => !newMealIngredients.Any(ni => ni.IngridientId.Equals(i.IngridientId)));
+        }
+
+        public async Task PostAsync(Meal meal)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://stirred-eagle-95.hasura.app/api/rest/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync($"ingridientsData?id={meal.Id}&name={meal.Name}");
+                if (response.StatusCode is HttpStatusCode.Created)
+                {
+                    Console.WriteLine("Meal created." + response.StatusCode);
+                }
+            }
         }
     }
 }
