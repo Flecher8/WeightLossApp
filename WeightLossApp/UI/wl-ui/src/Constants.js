d3f3f4605d0c33841205ead35532a2aa2cb56d92
Vlad Bocharov <vladislavboc11@gmail.com>
2022-07-05 15:45:26 +0300

    Changed IP v2

--- WeightLossApp/UI/wl-ui/src/Constants.js
+++ WeightLossApp/UI/wl-ui/src/Constants.js
@@ -1,5 +1,5 @@
 export const constants = {
-	API_URL: "https://192.168.1.197:45455/api/",
+	API_URL: "https://31.202.178.5:45455/api/",
 	API_Imgur: "https://api.imgur.com/3/image/",
 	Client_ID: "e9a6b3bb9d8860f"
 };
